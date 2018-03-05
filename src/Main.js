76f1ee38a288a0b4e68c342a3e9e855296390b41
DahengZhang <Daheng_Zhang@outlook.com>
2018-03-05 23:47:04 +0800

    剪切区域与剪切结果不符

--- src/Main.js
+++ src/Main.js
@@ -6,52 +6,77 @@ import { Cut } from './Cut.js';
 import { Draw } from './Draw.js'
 
 export class Main {
-   constructor(el, preview) {
-      el.onselectstart = new Function('event.returnValue=false;');
-      this.el = el;
-      this.preview = preview;
-      this.frame = Frame.getInstance(this.el);
-      this.calculation = new Calculation();
-      this.cut = new Cut();
-      this.init();
-   }
+    constructor(el, preview) {
+        el.onselectstart = new Function('event.returnValue=false;');
+        this.el = el;
+        this.preview = preview;
+        this.frame = Frame.getInstance(this.el);
+        this.calculation = new Calculation();
+        this.cut = new Cut();
+        this.init();
+    }
 
-   createPreviewCanvas() {
-      this.pCanvas = Canvas.create(this.preview.getBoundingClientRect());
-      this.pCtx = this.pCanvas.getContext('2d');
-      this.preview.appendChild(this.pCanvas);
-   }
+    createPreviewCanvas() {
+        this.pCanvas = Canvas.create(this.preview.getBoundingClientRect());
+        this.pCtx = this.pCanvas.getContext('2d');
+        this.preview.appendChild(this.pCanvas);
+    }
 
-   showImage(image) {
-      this.el.appendChild(image);
-      const [top, right, bottom, left] = this.calculation.calculation(image.getBoundingClientRect(), this.el.getBoundingClientRect());
-      if (top === 0) { }
-      image.style = `position: absolute;
-      z-index: 1;
-      top: ${top}px;
-      left: ${left}px;`
-   }
+    showImage(image) {
+        this.el.appendChild(image);
+        const [top, right, bottom, left] = this.calculation.calculation(image.getBoundingClientRect(), this.el.getBoundingClientRect());
+        let imageWidth = 0;
+        let imageHeight = 0;
+        if (top === 0) {
+            imageWidth = this.el.getBoundingClientRect().width - right - left;
+            imageHeight = this.el.getBoundingClientRect().height;
+            this.scale = image.getBoundingClientRect().width / imageWidth;
+        } else {
+            imageWidth = this.el.getBoundingClientRect().width;
+            imageHeight = this.el.getBoundingClientRect().height - top - bottom;
+            this.scale = image.getBoundingClientRect().height / imageHeight;
+        }
+        image.style = `position: absolute;
+        z-index: 1;
+        top: ${top}px;
+        left: ${left}px;
+        width: ${imageWidth}px;
+        height: ${imageHeight}px;`
+        this.frame.setMaxSize(top, right, bottom, left);
+        this.frameSize = [top, right, bottom, left];
+        this.drawPreview(...this.frame.getPoint());
+        this.windowEvent();
+    }
 
-   windowEvent() {
-      window.addEventListener('mousemove', e => {
-         e.stopPropagation();
-         if (this.frame.nowContact !== null) {
-            this.frame.changeSize(e);
-            this.drawPreview(...this.frame.getPoint());
-         }
-      });
-   }
+    windowEvent() {
+        window.addEventListener('mousemove', e => {
+            e.stopPropagation();
+            if (this.frame.nowContact !== null) {
+                this.frame.changeSize(e);
+                this.drawPreview(...this.frame.getPoint());
+            }
+        });
+    }
 
-   drawPreview(top, right, bottom, left) {
-      const cut = this.cut.cut(this.oImage.getBoundingClientRect(), this.el.getBoundingClientRect());
-      // console.log(top - this.frameSize[0], right - this.frameSize[1], bottom - this.frameSize[2], left - this.frameSize[3])
-      console.log(cut)
-   }
+    drawPreview(top, right, bottom, left) {
+        top = top * this.scale;
+        right = right * this.scale;
+        bottom = bottom * this.scale;
+        left = left * this.scale;
+        const srcX = left - this.frameSize[3];
+        const srcY = top - this.frameSize[0];
+        const srcW = this.image.getBoundingClientRect().width * this.scale - srcX - right + this.frameSize[1] * this.scale;
+        const srcH = this.image.getBoundingClientRect().height * this.scale - srcY - bottom + this.frameSize[2] * this.scale;
+        const d = this.cut.cut({ width: srcW, height: srcH }, this.preview.getBoundingClientRect());
+        this.pCtx.clearRect(0, 0, this.el.getBoundingClientRect().width, this.el.getBoundingClientRect().height);
+        Draw.drawImage(this.image, this.pCtx, srcX, srcY, srcW, srcH, ...d);
+    }
 
-   init() {
-      this.createPreviewCanvas();
-      Loader.onloaded('2.jpg').then(response => {
-         this.showImage(response);
-      });
-   }
+    init() {
+        this.createPreviewCanvas();
+        Loader.onloaded('0.jpg').then(response => {
+            this.image = response;
+            this.showImage(response);
+        });
+    }
 }
